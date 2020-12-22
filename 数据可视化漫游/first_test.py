2ca56bdca71246d25c59e5a089968409c980298a
AlexZ33 <zhaokang1@xiaomi.com>
2020-12-22 17:00:27 +0800

    init

---
+++ 数据可视化漫游/first_test.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time : 2020/12/22
+# @Author : AlexZ33
+# @Site :
+# @reference: https://www.analyticsvidhya.com/blog/2015/05/data-visualization-python/
+# https://www.analyticsvidhya.com/blog/2015/05/data-visualization-resource/
+# @File : first_test.py
+# @Software: PyCharm
+import matplotlib.pyplot as plt
